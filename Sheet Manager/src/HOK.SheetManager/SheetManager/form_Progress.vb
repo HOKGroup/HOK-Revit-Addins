68d496dba5fa9402d7c2afb888051d6666f1a49b
JinsolKim <jinsol.kim@hok.com>
2014-06-16 17:33:12 -0400

    Commit Sheet Manager Documentation

---
+++ Sheet Manager/src/HOK.SheetManager/SheetManager/form_Progress.vb
@@ -0,0 +1,34 @@
+﻿''' <summary>
+''' A simple progress floater
+''' </summary>
+''' <remarks></remarks>
+Public Class form_Progress
+
+    ''' <summary>
+    ''' Constructor
+    ''' </summary>
+    ''' <param name="maximum"></param>
+    ''' <remarks></remarks>
+    Public Sub New(ByVal maximum As Integer)
+        InitializeComponent()
+        progressBar1.Maximum = maximum
+    End Sub
+
+    ''' <summary>
+    ''' Increment one step
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub Increment()
+        progressBar1.Increment(1)
+    End Sub
+
+    ''' <summary>
+    ''' Reset the controls
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub Reset()
+        progressBar1.Value = 0
+        progressBar1.Refresh()
+    End Sub
+
+End Class
