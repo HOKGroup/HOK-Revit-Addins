15614533a0d4ac8e73089726b87f8c99634c7d04
JinsolKim <jinsol.kim@hok.com>
2014-06-23 11:14:11 -0400

    HOK Navigator 2015

--- Navigator/src/HOK.Navigator/HOK.Navigator/Properties/AssemblyInfo.cs
+++ Navigator/src/HOK.Navigator/HOK.Navigator/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HOK Navigator for Multi-versions of Revit")]
+[assembly: AssemblyTitle("HOK Navigator for Revit (Multi-Versions)")]
 [assembly: AssemblyDescription("This tool will provide links to access support pages and installers.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HOK Group")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.0.1.10")]
-[assembly: AssemblyFileVersion("2015.0.1.10")]
+[assembly: AssemblyVersion("2013.0.1.10")]
+[assembly: AssemblyFileVersion("2013.0.1.10")]
