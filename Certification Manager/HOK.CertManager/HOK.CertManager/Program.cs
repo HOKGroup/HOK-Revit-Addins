44ddd09c8dc76df4c71979260c024320454c3253
JinsolKim <jinsol.kim@hok.com>
2016-10-24 15:36:26 -0400

    Signed Addins for 2017
    Signed with HOK Authorized Certificates

---
+++ Certification Manager/HOK.CertManager/HOK.CertManager/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOK.CertManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                X509Certificate2 certificate = new X509Certificate2(@"hokaddin.pfx", "hokaddin", X509KeyStorageFlags.DefaultKeySet);
+                X509Store store = new X509Store(StoreName.TrustedPublisher, StoreLocation.LocalMachine);
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(certificate);
+                store.Close();
+
+                X509Store rootStore = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+                rootStore.Open(OpenFlags.ReadWrite);
+                rootStore.Add(certificate);
+                rootStore.Close();
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+            }
+        }
+    }
+}
