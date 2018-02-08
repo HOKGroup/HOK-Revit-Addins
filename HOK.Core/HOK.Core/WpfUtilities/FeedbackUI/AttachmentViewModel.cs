2200b7064f3a0b152fc3d9ced75a84aeaf72e30e
Konrad K Sobon <sobon.konrad@gmail.com>
2018-02-08 18:35:58 -0500

    add image attachment to feedback tool

---
+++ HOK.Core/HOK.Core/WpfUtilities/FeedbackUI/AttachmentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace HOK.Core.WpfUtilities.FeedbackUI
+{
+    public class AttachmentViewModel : ViewModelBase
+    {
+        public GalaSoft.MvvmLight.Command.RelayCommand Delete { get; set; }
+        public FeedbackViewModel ViewModel { get; set; }
+
+        public AttachmentViewModel(FeedbackViewModel vm)
+        {
+            ViewModel = vm;
+            Delete = new GalaSoft.MvvmLight.Command.RelayCommand(OnDelete);
+        }
+
+        private void OnDelete()
+        {
+            ViewModel.DeleteAttachment(this);
+        }
+
+        private string _name;
+        public string Name {
+            get { return _name; }
+            set { _name = value; RaisePropertyChanged(() => Name); }
+        }
+    }
+}
--- HOK.Core/HOK.Core/WpfUtilities/FeedbackUI/FeedbackViewModel.cs
+++ HOK.Core/HOK.Core/WpfUtilities/FeedbackUI/FeedbackViewModel.cs
@@ -1,18 +1,47 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
 using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
 
 namespace HOK.Core.WpfUtilities.FeedbackUI
 {
+    public class FilePathToFileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Path.GetFileName(System.Convert.ToString(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class FeedbackViewModel : ViewModelBase
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
         public string ToolName { get; set; }
         public string Title { get; set; }
         public FeedbackModel Model { get; set; }
         public RelayCommand<Window> WindowLoaded { get; }
+        public RelayCommand<KeyEventArgs> WindowKeyDown { get; }
         public RelayCommand<Window> Submit { get; }
         public RelayCommand<Window> Cancel { get; }
+        public GalaSoft.MvvmLight.Command.RelayCommand ChooseFile { get; }
+        public ObservableCollection<AttachmentViewModel> Attachments { get; set; } = new ObservableCollection<AttachmentViewModel>();
 
         public FeedbackViewModel(FeedbackModel model, string toolname)
         {
@@ -20,8 +49,84 @@ namespace HOK.Core.WpfUtilities.FeedbackUI
             ToolName = toolname;
             Title = "HOK Feedback Tool v." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
             WindowLoaded = new RelayCommand<Window>(OnWindowLoaded);
+            WindowKeyDown = new RelayCommand<KeyEventArgs>(OnWindowKeyDown);
             Submit = new RelayCommand<Window>(OnSubmit);
             Cancel = new RelayCommand<Window>(OnCancel);
+            ChooseFile = new GalaSoft.MvvmLight.Command.RelayCommand(OnChooseFile);
+        }
+
+        public void DeleteAttachment(AttachmentViewModel vm)
+        {
+            Attachments.Remove(vm);
+        }
+
+        private void OnChooseFile()
+        {
+            var file = SelectImageFile();
+            if (string.IsNullOrEmpty(file)) return;
+
+            var attachment = new AttachmentViewModel(this)
+            {
+                Name = Path.GetFileName(file)
+            };
+
+            Attachments.Add(attachment);
+        }
+
+        public static string SelectImageFile()
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = ".jpg",
+                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"
+            };
+            var result = dialog.ShowDialog();
+
+            return result != true ? string.Empty : dialog.FileName;
+        }
+
+        private void OnWindowKeyDown(KeyEventArgs args)
+        {
+            if (args.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    var clipboardData = Clipboard.GetDataObject();
+                    if (clipboardData != null)
+                    {
+                        if (clipboardData.GetDataPresent(DataFormats.Bitmap))
+                        {
+                            var bitmap = GetBitmap((BitmapSource)clipboardData.GetData(DataFormats.Bitmap));
+                            if (bitmap != null)
+                            {
+                                var hBitmap = bitmap.GetHbitmap();
+                                try
+                                {
+                                    var source = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                                }
+                                finally
+                                {
+                                    DeleteObject(hBitmap);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static Bitmap GetBitmap(BitmapSource source)
+        {
+            var bmp = new Bitmap(source.PixelWidth, source.PixelHeight, PixelFormat.Format32bppPArgb);
+            var data = bmp.LockBits(new Rectangle(System.Drawing.Point.Empty, bmp.Size), ImageLockMode.WriteOnly, PixelFormat.Format32bppPArgb);
+            source.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride);
+            bmp.UnlockBits(data);
+            return bmp;
         }
 
         private static void OnCancel(Window win)
