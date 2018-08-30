84e700efec42ab659014f245bbb18ed3c15db918
ksobon <ksobon1986@gmail.com>
2018-08-30 16:21:37 -0400

    publish file paths

---
+++ Project Monitor/src/HOK.MissionControl/HOK.MissionControl.Core/Schemas/FilePaths/FilePathItem.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace HOK.MissionControl.Core.Schemas.FilePaths
+{
+    public class FilePathItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("centralPath")]
+        public string CentralPath { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("projectId")]
+        public string ProjectId { get; set; }
+
+        [JsonConstructor]
+        public FilePathItem()
+        {
+        }
+
+        public FilePathItem(string centralPath)
+        {
+            CentralPath = centralPath.ToLower();
+        }
+    }
+}
