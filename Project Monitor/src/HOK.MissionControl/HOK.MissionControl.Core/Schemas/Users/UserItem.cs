670ceaa38c8f90cffd031a756722243856fa41ce
ksobon <ksobon1986@gmail.com>
2018-09-13 12:32:33 -0400

    publish user and machine info

---
+++ Project Monitor/src/HOK.MissionControl/HOK.MissionControl.Core/Schemas/Users/UserItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace HOK.MissionControl.Core.Schemas.Users
+{
+    public class UserItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("user")] public string User { get; set; }
+        [JsonProperty("machine")] public string Machine { get; set; }
+        [JsonProperty("createdAt")] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [JsonProperty("updatedAt")] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
