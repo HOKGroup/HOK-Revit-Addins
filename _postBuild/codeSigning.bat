f88d31e4f0d8dea8b746ebbf5026a5206d7e8130
Dan Siroky <dan.siroky@hok.com>
2019-09-06 16:37:27 -0500

    ci: Clean up CI files

--- _postBuild/codeSigning.bat
+++ _postBuild/codeSigning.bat
@@ -1,7 +1,7 @@
-REM @echo off
+@echo off
 echo.
 echo Signing Dlls...
-setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+setlocal ENABLEEXTENSIONS
 
 set DLL_PATH=%1
 REM set PFX_PATH="C:\Users\ksobon\source\repos\HOK-Revit-Addins\_cert\archilabCertificate.pfx"
@@ -9,10 +9,6 @@ set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\HOK"
 set VALUE_NAME="certificatePassword"
 
 REM for /f "tokens=3" %%a in ('reg query %KEY_NAME% /V %VALUE_NAME% ^|findstr /ri "REG_SZ"') do set PFX_PASS=%%a
-echo %DLL_PATH%
-echo %PFX_PASS%
-echo %PFX_PATH%
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode "%DLL_PATH%"
-REM "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool.exe"  sign /c "Code Signing - DTM" /v  %DLL_PATH%
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode "%DLL_PATH%"
 endlocal
