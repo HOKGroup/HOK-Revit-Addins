27d41640df7e2a6e1ad98bca2284341431535aa2
Dan Siroky <dan.siroky@hok.com>
2021-03-10 18:37:42 -0600

    Remove hard-coded signtool.exe path for Azure Pipelines build

--- _postBuild/codeSigning.bat
+++ _postBuild/codeSigning.bat
@@ -10,7 +10,7 @@ set VALUE_NAME="certificatePassword"
 
 REM for /f "tokens=3" %%a in ('reg query %KEY_NAME% /V %VALUE_NAME% ^|findstr /ri "REG_SZ"') do set PFX_PASS=%%a
 IF DEFINED BUILD_ENV (
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode "%DLL_PATH%"
+"signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode "%DLL_PATH%"
 ) ELSE (
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /c "Code Signing - DTM" /t http://timestamp.comodoca.com/authenticode %DLL_PATH%
 )
