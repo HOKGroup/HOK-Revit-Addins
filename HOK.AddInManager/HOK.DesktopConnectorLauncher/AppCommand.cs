a4ef0773430e581bc353dd9cde7f3a1f2d1b8700
Dan Siroky <dan.siroky@hok.com>
2021-05-07 09:21:39 -0500

    Adds Citrix Desktop Connector launcher add-in

---
+++ HOK.AddInManager/HOK.DesktopConnectorLauncher/AppCommand.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Events;
+using HOK.Core.Utilities;
+using Microsoft.Win32;
+
+namespace HOK.DesktopConnectorLauncher
+{
+    public class AppCommand : IExternalApplication
+    {
+        private UIControlledApplication m_app;
+        private string tabName = "";
+        private string currentAssembly = "";
+        public PushButton helpButton;
+        public PushButton desktopConnectorButton;
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            m_app = application;
+            tabName = "   HOK   ";
+            currentAssembly = Assembly.GetAssembly(GetType()).Location;
+
+            application.ControlledApplication.ApplicationInitialized += EventAppInitialize;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            application.ControlledApplication.ApplicationInitialized -= EventAppInitialize;
+
+            return Result.Succeeded;
+        }
+
+        private void EventAppInitialize(object sender, ApplicationInitializedEventArgs arg)
+        {
+            // Get registry key
+            string keyName = @"HKEY_CURRENT_USER\SOFTWARE\HOK\BIM\";
+            string valueName = @"DesktopConnectorPath_Citrix";
+            string path = (string)Registry.GetValue(keyName, valueName, null);
+            if (path != null && System.IO.File.Exists(path)) {
+                CreatePushButton();
+            }
+            // If exists + is valid path
+        }
+
+        /// <summary>
+        /// Creates push button for Desktop Connector.
+        /// </summary>
+        private void CreatePushButton()
+        {
+            try
+            {
+                try
+                {
+                    m_app.CreateRibbonTab(tabName);
+                }
+                catch (Exception ex)
+                {
+                    Log.AppendLog(LogMessageType.EXCEPTION, ex.Message);
+                }
+
+                var created = m_app.GetRibbonPanels(tabName).FirstOrDefault(x => x.Name == " Addins ");
+                var addinsPanel = created ?? m_app.CreateRibbonPanel(tabName, " Addins ");
+                var assembly = Assembly.GetExecutingAssembly();
+                var iconImage = LoadBitmapImage(assembly, "desktopConnector.png");
+                var addinManagerButton = addinsPanel.GetItems().FirstOrDefault();
+
+                var buttonData = new PushButtonData("HOK Desktop Connector Launcher", "Launch Autodesk" + Environment.NewLine + "Desktop Connector ", currentAssembly, "HOK.DesktopConnectorLauncher.DesktopConnectorCommand")
+                {
+                    AvailabilityClassName = "HOK.DesktopConnectorLauncher.Availability",
+                    LargeImage = iconImage,
+                    Image = iconImage
+                };
+
+                if (addinManagerButton != null)
+                {
+                    SplitButtonData splitButtonData = new SplitButtonData("Addins", "Text");
+                    SplitButton splitButton = addinsPanel.AddItem(splitButtonData) as SplitButton;
+                    var pbAddinManager = addinManagerButton as PushButton;
+                    PushButtonData newAddinManagerButton = new PushButtonData(pbAddinManager.Name, pbAddinManager.ItemText, pbAddinManager.AssemblyName, pbAddinManager.ClassName)
+                    {
+                        AvailabilityClassName = pbAddinManager.AvailabilityClassName,
+                        LargeImage = pbAddinManager.LargeImage,
+                        Image = pbAddinManager.Image,
+                        ToolTip = pbAddinManager.ToolTip
+                        
+                    };
+                    newAddinManagerButton.SetContextualHelp(pbAddinManager.GetContextualHelp());
+                    pbAddinManager.Enabled = false;
+                    pbAddinManager.Visible = false;
+                    splitButton.AddPushButton(newAddinManagerButton);
+                    splitButton.AddPushButton(buttonData);
+
+                } else
+                {
+                    desktopConnectorButton = addinsPanel.AddItem(buttonData) as PushButton; 
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.AppendLog(LogMessageType.EXCEPTION, ex.Message);
+            }
+        }
+
+        private static BitmapImage LoadBitmapImage(Assembly assembly, string imageName)
+        {
+            var image = new BitmapImage();
+            try
+            {
+                var prefix = typeof(AppCommand).Namespace + ".Resources.";
+                var stream = assembly.GetManifestResourceStream(prefix + imageName);
+
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.EndInit();
+            }
+            catch (Exception ex)
+            {
+                Log.AppendLog(LogMessageType.EXCEPTION, ex.Message);
+            }
+            return image;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Availability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, Autodesk.Revit.DB.CategorySet selectedCategories)
+        {
+            return true;
+        }
+    }
+}
---
+++ HOK.AddInManager/HOK.DesktopConnectorLauncher/DesktopConnectorCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using HOK.Core.Utilities;
+using HOK.MissionControl.Core.Schemas;
+using HOK.MissionControl.Core.Utils;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace HOK.DesktopConnectorLauncher
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class DesktopConnectorCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, Autodesk.Revit.DB.ElementSet elements)
+        {
+            Log.AppendLog(LogMessageType.INFO, "Started");
+
+            try
+            {
+                // (Konrad) We are gathering information about the addin use. This allows us to
+                // better maintain the most used plug-ins or discontiue the unused ones.
+                string keyName = @"HKEY_CURRENT_USER\SOFTWARE\HOK\BIM\";
+                string valueName = "DesktopConnectorPath_Citrix";
+                string path = (string)Registry.GetValue(keyName, valueName, null);
+                if (path != null && System.IO.File.Exists(path)) {
+                    Process.Start(path);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.AppendLog(LogMessageType.EXCEPTION, e.Message);
+            }
+
+            Log.AppendLog(LogMessageType.INFO, "Ended");
+            return Result.Succeeded;
+        }
+    }
+}
