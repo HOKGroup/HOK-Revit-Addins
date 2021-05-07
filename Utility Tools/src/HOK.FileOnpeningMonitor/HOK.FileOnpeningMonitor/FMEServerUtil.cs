74690b4c40fe4b89c1364c192ef56b9b6ec70dbd
Dan Siroky <dan.siroky@hok.com>
2021-05-07 14:03:39 -0500

    Remove FME Server .NET API references

--- Utility Tools/src/HOK.FileOnpeningMonitor/HOK.FileOnpeningMonitor/FMEServerUtil.cs
+++ Utility Tools/src/HOK.FileOnpeningMonitor/HOK.FileOnpeningMonitor/FMEServerUtil.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Windows;
 using RestSharp;
 using HOK.Core.Utilities;
-using Safe.FMEServer.API;
 using System.Net;
 
 namespace HOK.FileOnpeningMonitor
@@ -17,46 +16,6 @@ namespace HOK.FileOnpeningMonitor
         private static string host = "";
         private static int port;
         private static string clientId = "";
-
-        private static IFMEServerSession serverSession;
-        private static IFMERepositoryManager repositoryMgr;
-
-        public static bool RunFMEWorkspace(CentralFileInfo info, string repository, string workspace, out Dictionary<string, string> properties)
-        {
-            var result = false;
-            properties = new Dictionary<string, string>();
-            try
-            {
-                if (null == serverSession)
-                {
-                    serverSession = ConnectToFMEServer();
-                }
-
-                if (null != serverSession)
-                {
-                    var transformationMgr = serverSession.GetTransformationManager();
-
-                    var request = serverSession.CreateTransformationRequest("SERVER_CONSOLE_CLIENT", repository, workspace);
-                    request.SetPublishedParameter("Office_pp", info.UserLocation);
-                    request.SetPublishedParameter("Username_pp", info.UserName);
-                    request.SetPublishedParameter("FileName_pp", info.DocCentralPath);
-
-                    var transformationResult = serverSession.CreateTransformationResult();
-                    var jobId = transformationMgr.SubmitJob(request);
-                    Thread.Sleep(500);
-
-                    if (transformationMgr.GetJobResult(jobId, transformationResult))
-                    {
-                        result = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to run FME workspace from the FME Server.\n" + ex.Message, "Run FME Workspace", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            return result;
-        }
 
         public static bool RunFMEWorkspaceHTTP(CentralFileInfo info, string repository, string workspace, out Dictionary<string, string> properties)
         {
@@ -99,32 +58,5 @@ namespace HOK.FileOnpeningMonitor
             return result;
         }
 
-        private static IFMEServerSession ConnectToFMEServer()
-        {
-            IFMEServerSession fmeServerSession;
-            try
-            {
-                var settingsString = Resources.StreamEmbeddedResource("HOK.Core.Resources.Settings.json");
-                var settings = Json.Deserialize<Settings>(settingsString);
-
-                userId = settings.FileOnOpeningFmeUserId;
-                password = settings.FileOnOpeningFmePassword;
-                host = settings.FileOnOpeningFmeHost;
-                port = settings.FileOnOpeningFmePort;
-                clientId = settings.FileOnOpeningFmeClientId;
-                fmeServerSession = FMEServer.CreateServerSession();
-
-                var connectInfo = fmeServerSession.CreateServerConnectionInfo(host, port, userId, password);
-                fmeServerSession.Init(connectInfo, null);
-
-                repositoryMgr = fmeServerSession.GetRepositoryManager();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to connect to FME Server.\n" + ex.Message, "Connect to FME Server", MessageBoxButton.OK, MessageBoxImage.Warning);
-                fmeServerSession = null;
-            }
-            return fmeServerSession;
-        }
     }
 }
