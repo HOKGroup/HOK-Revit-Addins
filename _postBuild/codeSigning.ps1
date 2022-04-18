4905b684a7d8ee95e3dbcdf1d063158e3920d5db
Dan Siroky <dan.siroky@hok.com>
2022-04-18 17:02:51 -0500

    Hard code temp

--- _postBuild/codeSigning.ps1
+++ _postBuild/codeSigning.ps1
@@ -3,7 +3,8 @@
 $DLL_PATH = $args[0]
 
 If ($Env:BUILD_ENV -eq 'AzureDevOps') {
-    &"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f $Env:PFX_PATH /p $Env:PFX_PASS /t http://timestamp.comodoca.com/authenticode $DLL_PATH
+    $p = 'abc123'
+    &"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f $Env:PFX_PATH /p $p /t http://timestamp.comodoca.com/authenticode $DLL_PATH
 } Else {
     &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /c '2021 Code Signing - DTM ' /t http://timestamp.comodoca.com/authenticode $DLL_PATH
 }
