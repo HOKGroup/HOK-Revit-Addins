ea3f63536dfbcd6303520c0616d776c8bb0d9553
Dan Siroky <dan.siroky@hok.com>
2022-04-18 16:14:15 -0500

    Debug PowerShell script

--- _postBuild/codeSigning.ps1
+++ _postBuild/codeSigning.ps1
@@ -3,7 +3,11 @@
 $DLL_PATH = $args[0]
 
 If ($Env:BUILD_ENV -eq 'AzureDevOps') {
-    &"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f $Env:PFX_PATH /p $Env:PFX_PASS /t http://timestamp.comodoca.com/authenticode $DLL_PATH
+    Write-Output "PFX_PATH: $Env:PFX_PATH"
+    Write-Output "PFX_PASS: $Env:PFX_PASS"
+    $s = '"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f $Env:PFX_PATH /p "$Env:PFX_PASS" /t http://timestamp.comodoca.com/authenticode $DLL_PATH'
+    Write-Output "COMMAND: $s"
+    &"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /f $Env:PFX_PATH /p "$Env:PFX_PASS" /t http://timestamp.comodoca.com/authenticode $DLL_PATH
 } Else {
     &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /c '2021 Code Signing - DTM ' /t http://timestamp.comodoca.com/authenticode $DLL_PATH
 }
