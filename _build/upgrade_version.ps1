3323622b8f9b16bbefedf0a6948270f4bab32539
Dan Siroky <dan.siroky@hok.com>
2023-06-16 11:39:16 -0500

    Upgrade script: CSproj section (WIP)

--- _build/upgrade_version.ps1
+++ _build/upgrade_version.ps1
@@ -1,16 +1,16 @@
-## Add new version to .sln files
-##$version = $args[0]
+$version = $args[0]
 
+#================= Add new version to .sln files ===================
 ## Find all .sln files
 $slnFiles = Get-ChildItem -Path . -Filter *.sln -Recurse
 
 $SlnSearchExp = '(?<DebugLine>(?<Indent>\s+)Debug\|x64 = Debug\|x64)'
-$SlnReplaceExp = '${Indent}2024|x64 = 2024|x64
+$SlnReplaceExp = '${Indent}' + $([regex]::escape($version)) + '|x64 = ' + $([regex]::escape($version)) + '|x64
 ${DebugLine}'
 
 $ProjectSearchExp = '(?<DebugLine>(?<Indent>\s+)\{((?<ProjectId>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})\})\.Debug\|x64\.(ActiveCfg)(\.0)? = Debug\|x64)'
-$ProjectReplaceExp = '${Indent}{${ProjectId}}.2024|x64.ActiveCfg = 2024|x64
-${Indent}{${ProjectId}}.2024|x64.Build.0 = 2024|x64
+$ProjectReplaceExp = '${Indent}{${ProjectId}}.' + $([regex]::escape($version)) + '|x64.ActiveCfg = ' + $([regex]::escape($version)) + '|x64
+${Indent}{${ProjectId}}.' + $([regex]::escape($version)) + '|x64.Build.0 = ' + $([regex]::escape($version)) + '|x64
 ${Indent}{${ProjectId}}.Debug|x64.ActiveCfg = Debug|x64'
 
 foreach ($slnFile in $slnFiles) {
@@ -20,4 +20,20 @@ foreach ($slnFile in $slnFiles) {
     (Get-Content $slnFile) | ForEach-Object { 
         $_ -replace $ProjectSearchExp, $ProjectReplaceExp 
     } | Set-Content $slnFile
+}
+
+#================= Add new version to .csproj files ===================
+$csprojFiles = Get-ChildItem -Path . -Filter *.csproj -Recurse
+$Namespace = @{msbuild ='http://schemas.microsoft.com/developer/msbuild/2003'}
+foreach ($csprojFile in $csprojFiles[0..1]) {
+    [xml]$xml = Get-Content $csprojFile
+    $c = Select-Xml $csprojFile -Namespace $Namespace -XPath '//msbuild:Project/msbuild:PropertyGroup'
+
+    foreach ($node in $c) {
+        $v = Select-Xml -Xml $node.node -XPath '//RevitVersion'
+        $v.Node.InnerText
+    }
+    # $c | Format-Table @{Label="Name"; Expression= {($_.node.innerxml).trim()}}, Path -AutoSize
+    # $a = Select-Xml -Xml $xml
+
 }
