d950446b7f48009deed61d3a5a38f7c5aceb5bd6
Dan Siroky <dan.siroky@hok.com>
2023-06-22 15:19:23 -0500

    Update auto-upgrade script

--- _build/upgrade_version.ps1
+++ _build/upgrade_version.ps1
@@ -1,4 +1,5 @@
 $version = $args[0]
+$prevVersion = "2023"
 
 #================= Add new version to .sln files ===================
 ## Find all .sln files
@@ -25,15 +26,48 @@ foreach ($slnFile in $slnFiles) {
 #================= Add new version to .csproj files ===================
 $csprojFiles = Get-ChildItem -Path . -Filter *.csproj -Recurse
 $Namespace = @{msbuild ='http://schemas.microsoft.com/developer/msbuild/2003'}
-foreach ($csprojFile in $csprojFiles[0..1]) {
-    [xml]$xml = Get-Content $csprojFile
-    $c = Select-Xml $csprojFile -Namespace $Namespace -XPath '//msbuild:Project/msbuild:PropertyGroup'
-
-    foreach ($node in $c) {
-        $v = Select-Xml -Xml $node.node -XPath '//RevitVersion'
-        $v.Node.InnerText
+[System.Xml.XmlNamespaceManager]$ns = New-Object System.Xml.XmlNamespaceManager(New-Object System.Xml.NameTable)
+$ns.AddNamespace('msbuild', $Namespace.msbuild)
+foreach ($csprojFile in $csprojFiles) {
+    Write-Host "Updating $csprojFile"
+    $XPath = '//msbuild:Project/msbuild:PropertyGroup[contains(@Condition, "' + $prevVersion + '")]'
+    Write-Host $XPath
+    $previousVersionXml = Select-Xml $csprojFile -Namespace $Namespace -XPath $XPath
+    if ($null -eq $previousVersionXml.Node) {
+        Write-Host "No previous version found in $csprojFile"
+        continue
     }
-    # $c | Format-Table @{Label="Name"; Expression= {($_.node.innerxml).trim()}}, Path -AutoSize
-    # $a = Select-Xml -Xml $xml
-
+    if ($previousVersionXml.Node.Count -gt 1) {
+        Write-Host "Multiple previous versions found in $csprojFile"
+        $lastInsertedNode = $previousVersionXml.Node[$previousVersionXml.Node.Count - 1]
+        foreach ($node in $previousVersionXml.Node) {
+            $newVersionXml = $node.Clone()
+            ## Change values in specific fields with version number in it
+            if ($newVersionXml.HasAttribute('Condition')) {
+                $newCondition = $newVersionXml.Condition -replace $prevVersion, $version
+                $newVersionXml.SetAttribute('Condition', $newCondition)
+            }
+            $childrenToChange = @("OutputPath", "RevitVersion", "DefineConstants")
+            foreach ($child in $childrenToChange) {
+                $childNode = $newVersionXml.SelectSingleNode(".//msbuild:$child", $ns)
+                $childNode.InnerText = $childNode.InnerText -replace $prevVersion, $version
+            }
+            $lastInsertedNode.ParentNode.InsertAfter($newVersionXml, $lastInsertedNode)
+            $lastInsertedNode = $newVersionXml
+        }
+    } else {
+        $newVersionXml = $previousVersionXml.Node.Clone()
+        ## Change values in specific fields with version number in it
+        if ($newVersionXml.HasAttribute('Condition')) {
+            $newCondition = $newVersionXml.Condition -replace $prevVersion, $version
+            $newVersionXml.SetAttribute('Condition', $newCondition)
+        }
+        $childrenToChange = @("OutputPath", "RevitVersion", "DefineConstants")
+        foreach ($child in $childrenToChange) {
+            $childNode = $newVersionXml.SelectSingleNode(".//msbuild:$child", $ns)
+            $childNode.InnerText = $childNode.InnerText -replace $prevVersion, $version
+        }
+        $previousVersionXml.Node.ParentNode.InsertAfter($newVersionXml, $previousVersionXml.Node)
+    }
+    Set-Content $csprojFile $newVersionXml.OwnerDocument.OuterXml
 }
