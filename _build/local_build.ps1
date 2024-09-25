0959f51bad04293aebd985b20873c7e600d6977e
Dan Siroky <dan.siroky@hok.com>
2024-09-25 12:53:54 -0500

    Add local build script, update resources

---
+++ _build/local_build.ps1
@@ -0,0 +1,33 @@
+# Usage
+# .\_build\local_build.ps "2025" "Debug 25"
+
+$ErrorActionPreference = 'Continue'
+$revitVersion = $args[0]
+$buildConfiguration = $args[1]
+$env:BUILD_CONFIGURATION = $revitVersion
+$sourceDir = $pwd
+
+
+## Copy Resources to artifacts folder
+& "$sourceDir/_build/copy_resources.ps1"
+
+dotnet build "\HOK.Core\HOK.Core.sln" -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+dotnet build "HOK.MissionControl\HOK.MissionControl.Core\HOK.MissionControl.Core.csproj" -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+dotnet build "HOK.MissionControl\HOK.MissionControl\HOK.MissionControl.csproj" -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+dotnet build "HOK.Feedback\HOK.Feedback.sln" -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+dotnet build "HOK.MissionControl\HOK.MissionControl.sln" -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+$solutions = Get-ChildItem . -Filter "HOK.*.sln" -Recurse -Depth 2 -Exclude HOK.MissionControl*,HOK.Core*,HOK.Feedback*,HOK.ParameterTools*,HOK.FileOpeningMonitor*,HOK.ModelReporting*
+
+foreach ($solution in $solutions) {
+    dotnet build $solution.FullName -c "$buildConfiguration" --artifacts-path "$sourceDir/_artifacts" --property:OutputPath="$sourceDir/_artifacts/$revitVersion"
+
+}
+
+& "$sourceDir/_build/cleanup_artifacts.ps1"
+
+Remove-Item Env:\BUILD_CONFIGURATION
