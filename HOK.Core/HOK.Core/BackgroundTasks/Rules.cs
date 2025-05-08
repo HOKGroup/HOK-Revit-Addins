3bc5e6970d92042c5d1517f57ea2c18f759dc74d
Mohsen Assaqqaf <mohsen.assaqqaf@hok.com>
2025-05-08 11:11:18 -0400

    Removed a redundant variable

--- HOK.Core/HOK.Core/BackgroundTasks/Rules.cs
+++ HOK.Core/HOK.Core/BackgroundTasks/Rules.cs
@@ -88,21 +88,12 @@ namespace HOK.Core.BackgroundTasks
                     ss => ss.Name == "_HOK Automated Print Set"
                 );
 
-            // Get all of the sheets in the document
-            List<ElementId> sheetsInDocIds = new FilteredElementCollector(doc)
+            List<ElementId> sheetsInModel = new FilteredElementCollector(doc)
                 .OfClass(typeof(ViewSheet))
                 .ToElements()
                 .Cast<ViewSheet>()
                 .Select(s => s.Id)
                 .ToList();
-
-            List<ElementId> sheetsInModel = new List<ElementId>();
-
-            foreach (ElementId viewId in sheetsInDocIds)
-            {
-                View view = (View)doc.GetElement(viewId);
-                sheetsInModel.Add(viewId);
-            }
 
             // Add the excluded sheets to the sheet set
             ViewSet newVSS = new ViewSet();
